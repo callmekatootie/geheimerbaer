b9fe9d3a8d6285b350c71c3ae9c831391dd6a39b
Mithun Kamath <callmekatootie@gmail.com>
2020-12-02 23:04:15 +0530

    Fix spacing

--- client/src/views/Index.vue
+++ client/src/views/Index.vue
@@ -2,8 +2,8 @@
   <section class="hero is-light is-fullheight-with-navbar">
     <div class="hero-body">
       <div class="container">
-        <p class="title has-text-centered	">Geheimerbaer</p>
-        <p class="subtitle has-text-centered	">Welcome to Geheimerbaer</p>
+        <p class="title has-text-centered">Geheimerbaer</p>
+        <p class="subtitle has-text-centered">Welcome to Geheimerbaer</p>
       </div>
     </div>
   </section>
